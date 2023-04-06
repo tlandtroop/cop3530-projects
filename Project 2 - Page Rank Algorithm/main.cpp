84131f3055b185c4e2c2f5590d64528ec3e1b5ee
Tyler Landtroop <tlandtroop@ufl.edu>
2023-04-05 22:46:20 -0400

    started project 2

--- Project 2 - Page Rank Algorithm/main.cpp
+++ Project 2 - Page Rank Algorithm/main.cpp
@@ -1,1 +1,68 @@
-int main() { return 0; }
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class AdjacencyList {
+private:
+  // Stores our pages in a map
+  map<string, vector<pair<string, double>>> pages;
+
+public:
+  void addEdge(string &from, string &to);
+  void PageRank(int powerIterations);
+};
+
+void AdjacencyList::addEdge(string &from, string &to) {
+  // Adds an edge from the "from" page to the "to" page with a given weight
+  pages[from].push_back(make_pair(to, 1.0));
+}
+
+void AdjacencyList::PageRank(int powerIterations) {
+  int n = pages.size();
+
+  map<string, double> ranks;
+  for (auto page : pages) {
+    ranks[page.first] = 1.0 / n;
+  }
+
+  for (int i = 0; i < powerIterations; i++) {
+    map<string, double> newRanks;
+
+    for (auto page : pages) {
+      double newRank = 0.0;
+      for (auto link : page.second) {
+        newRank += ranks[link.first] / link.second;
+      }
+      newRanks[page.first] = newRank;
+    }
+
+    ranks = newRanks;
+  }
+
+  for (auto page : ranks) {
+    cout << page.first << " " << fixed << setprecision(2) << page.second
+         << endl;
+  }
+}
+
+int main() {
+  int numLines, powerIterations;
+  string from, to;
+
+  cin >> numLines;
+  cin >> powerIterations;
+
+  AdjacencyList graph;
+
+  for (int i = 0; i < numLines; i++) {
+    cin >> from;
+    cin >> to;
+    graph.addEdge(from, to);
+  }
+
+  graph.PageRank(powerIterations);
+
+  return 0;
+}
